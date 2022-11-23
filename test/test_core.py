93f5fde48adb44693844fd3b17847df5f0efd1fa
frank <franktian424@qq.com>
2022-11-23 22:12:02 +0800

    :bug: fixed test

--- test/test_core.py
+++ test/test_core.py
@@ -10,7 +10,7 @@ def test_env_params_name():
     env = EmeiEnv(env_params={"a": 3, "b": 5, "d": 0.33, "c": "c"})
     assert env.env_params_name == "a=3&b=5&c=c&d=0.33"
 
-    env = EmeiEnv(env_params=dict(freq_rate=1, time_step=0.02))
+    env = EmeiEnv(env_params=dict(freq_rate=1, real_time_scale=0.02))
     assert env.env_params_name == "freq_rate=1&real_time_scale=0.02"
 
 
